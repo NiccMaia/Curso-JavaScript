a86fdb3fe0c22dc0404148048b0ad18db25f8749
Nicolle Maia <nicolleluisamaiagurgel@gmail.com>
2024-02-27 19:19:09 -0300

    aula 2

---
+++ Aulas/Aula[002]/comentarios.js
@@ -0,0 +1,10 @@
+// Escrevendo um comentário
+console.log('Olá Mundo'); //Aqui possui um outro comentário
+console.log ('linha 1');
+//console.log ('linha 2');
+console.log ('linha 3');
+console.log ('linha 4');
+/*
+Um bloco de código
+Linha de comentário
+*/
