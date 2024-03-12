e2dd0a42a7f73b63526fad965b748d3be105283a
Nicolle Maia <nicolleluisamaiagurgel@gmail.com>
2024-03-12 11:00:55 -0300

    Aula 5 CONST

--- Seção [001]; JavaScript Básico - Para iniciantes/Aula[005]; Constantes com CONST/index.js
+++ Seção [001]; JavaScript Básico - Para iniciantes/Aula[005]; Constantes com CONST/index.js
@@ -2,6 +2,13 @@ const nome = 'João';
 console.log(nome);
 // Para descobrir qual o tipo de qualquer variavel do progama
 
+// = - * /
 const primeiroNumero = 5;
 const segundoNumero = 10;
-const conta = primeiroNumero * segundoNumero;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(primeiroNumero + segundoNumero);
+
+// Tentar sempre manter o valor inical da variavel, e ir evoluindo a mesma ao inves de ficar modificando o valor dela por meio do codigo.
